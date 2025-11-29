7420dd21bfa409f9567319e49777db4fab1b1164
AlexCuke <alexcuke@gmail.com>
2025-11-29 15:49:25 +0300

    Final

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 import requests
 import json
-import pprint as pp
 import os
+import tempfile
+
 # Кот от CATAAS 
 text = input("Введите текст: ").strip()
 url = f"https://cataas.com/cat/says/{text}?json=true"
 
+print("Соединениt с сайтом cataas.com установлено")
 response = requests.get(url)
 response.raise_for_status()
-print("Соединениt с сайтом cataas.com установлено")
 data = response.json()
-print(f"JSON загружен") # Сохранить JSON в файл 
-pp.pprint(data)     # Печатаем JSON в файл 
+print(f"JSON с данными кота получен") # Сохранить JSON в файл 
 
 cat_img_url =data["url"]
-img_response = requests.get(data["url"])
+img_response = requests.get(cat_img_url)
 img_response.raise_for_status()
+file_size = len(img_response.content)
+data["file_size_bytes"] = file_size
+
+json_filename = f"{text}.json"
 
 
 #Соединяем с яндесом
-api_key=""
+api_key = os.getenv("YADISK_TOKEN")
 ya_url= "https://cloud-api.yandex.net/v1/disk"
 headers = {
     "Authorization": f"OAuth {api_key}",
@@ -42,7 +46,7 @@ if resp.status_code not in (201, 409):
     print("Ошибка создания папки:", resp.status_code, resp.text)
     resp.raise_for_status()
 
-# Загрузить файл
+# Загрузить файл Картинки
 print("Загружаем файл на Яндекс.Диск")
 resp = requests.post(
     f"{ya_url}/resources/upload",
@@ -53,7 +57,32 @@ resp = requests.post(
         "overwrite": "true",
     },
 )
-print("status:", resp.status_code, resp.text)
+if resp.status_code != 202:
+    print("Ошибка загрузки на Диск:", resp.status_code, resp.text)
+    resp.raise_for_status()
+print("Файл загружен  на Яндекс.Диск:", disk_file_path)
+disk_json_path = folder_path + "/" + f"{text}.json"
 
+print("Загружаем файл JSON на Яндекс.Диск")
 
-print("Файл загружен на Яндекс.Диск:", disk_file_path)
+# 1. Запрашиваем href для загрузки
+r = requests.get(
+    f"{ya_url}/resources/upload",
+    headers=headers,
+    params={"path": disk_json_path, "overwrite": "true"},
+)
+r.raise_for_status()
+href = r.json()["href"]  # ссылка для PUT [web:21][web:7]
+
+# 2. Пишем JSON во временный файл и отправляем его
+with tempfile.NamedTemporaryFile(mode="w+b", delete=True) as tmp:
+    # пишем JSON как текст в байтовый файл
+    json_bytes = json.dumps(data, ensure_ascii=False, indent=2).encode("utf-8")
+    tmp.write(json_bytes)
+    tmp.flush()
+    tmp.seek(0)  # перемотать в начало, важно [web:24][web:33]
+
+    put_r = requests.put(href, data=tmp)
+    put_r.raise_for_status()
+
+print("JSON загружен на Яндекс.Диск:", disk_json_path)
